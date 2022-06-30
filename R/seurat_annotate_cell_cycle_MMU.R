9adef622c30339d2045ba7dafce638a2370e4ba9
Denis O'Meally <domeally@coh.org>
2022-06-30 10:21:56 -0700

    use seurat function
    Former-commit-id: 1c1134936cc78212858e51bd68f7f10e77c07808

--- R/seurat_annotate_cell_cycle_MMU.R
+++ R/seurat_annotate_cell_cycle_MMU.R
@@ -3,52 +3,61 @@
 #'
 #' Get the mouse cell cycle markers from the package `scran`, and annotate the
 #' Seutat object with the cell cycle labels for each cell.
-#' 
+#'
 #' See <https://ucdavis-bioinformatics-training.github.io/2019-single-cell-RNA-sequencing-Workshop-UCD_UCSF/scrnaseq_analysis/scRNA_Workshop-PART1.html>
 #' for more information.
 #'
 #' @name seurat_annotate_cell_cycle
-#' @param integrated A Seurat object to annotate
-#' @return A Surat object with cell cycle annotations
+#' @param seurat_object A Seurat object to annotate
+#' @return A Surat object with cell cycle annotations (adds `S.Score`, `G2M.Score` and `Phase` to the cell metadata columns)
 #' @author Denis O'Meally
 #' @export
-seurat_annotate_cell_cycle <- function(integrated) {
-    requireNamespace("org.Mm.eg.db")
+seurat_annotate_cell_cycle <- function(seurat_object) {
+    # https://rdrr.io/cran/Seurat/man/CellCycleScoring.html
+    # https://github.com/satijalab/seurat/issues/2493
 
+    # Previously used this with success:
     # https://ucdavis-bioinformatics-training.github.io/2019-single-cell-RNA-sequencing-Workshop-UCD_UCSF/scrnaseq_analysis/scRNA_Workshop-PART1.html
-    # Calculate cell cycle, add to meta data
-    # Using the package scran, get the mouse cell cycle markers and a mapping of m
+    # But its a bit hacky and overly complex. Seurat has a built in function for this.
 
-    # set DefaultAssay
-    Seurat::DefaultAssay(integrated) <- "RNA"
+    # ############################################################################
+    # ### This is the recommenced way to convert to mouse ids, but ensembl is down at time of testing
+    # # Basic function to convert human to mouse gene names
+    # convert_human_gene_list <- function(x) {
+    #     # https://www.r-bloggers.com/2016/10/converting-mouse-to-human-gene-names-with-biomart-package/
 
-    # Convert to matrix for use by cycle
-    mat <- as.matrix(Seurat::GetAssayData(integrated))
+    #     human <- biomaRt::useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+    #     mouse <- biomaRt::useMart("ensembl", dataset = "mmusculus_gene_ensembl")
 
-    # Get cell cycle markers
-    mm_pairs <- readRDS(system.file("exdata", "mouse_cycle_markers.rds", package = "scran"))
+    #     genesV2 <- biomaRt::getLDS(
+    #         attributes = c("hgnc_symbol"),
+    #         filters = "hgnc_symbol",
+    #         values = x,
+    #         mart = human,
+    #         attributesL = c("mgi_symbol"),
+    #         martL = mouse,
+    #         uniqueRows = T
+    #     )
 
-    #match EnsemblIDs to MGI symbols
-    anno_data <- clusterProfiler::bitr(rownames(mat), fromType = "SYMBOL", toType = "ENSEMBL", OrgDb = "org.Mm.eg.db")
-    row_order <- match(rownames(mat), anno_data$SYMBOL)
-    rownames(mat) <- anno_data$ENSEMBL[row_order]
+    #     humanx <- unique(genesV2[, 2])
 
-    #drop rows with no Ensembl ID
-    drop <- which(is.na(rownames(mat)))
-    mat <- mat[-drop, ]
+    #     # Print the first 6 genes found to the screen
+    #     print(head(humanx))
+    #     return(humanx)
+    # }
 
-    #calculate cell cycle
-    cycles <- scran::cyclone(mat, pairs = mm_pairs, BPPARAM = BiocParallel::MulticoreParam(future::availableCores()))
+    # g2m.genes <- convert_human_gene_list(Seurat::cc.genes$g2m.genes)
+    # s.genes <- convert_human_gene_list(Seurat::cc.genes$s.genes)
 
-    #add to meta data
-    tmp <- data.frame(cell_cycle = cycles$phases)
-    rownames(tmp) <- colnames(mat)
-    integrated <- Seurat::AddMetaData(integrated, tmp)
+    # In the interim, just make them all look like mice symbols
+    g2m_genes <- gsub("(?<=\\b)([a-z])", "\\U\\1", tolower(Seurat::cc.genes$g2m.genes), perl = TRUE)
+    s_genes <- gsub("(?<=\\b)([a-z])", "\\U\\1", tolower(Seurat::cc.genes$s.genes), perl = TRUE)
 
-    #Make a DimPLot
-    # set default Assay & identity -----------------------------------------------------
-    Seurat::DefaultAssay(integrated) <- "RNA"
-    Seurat::Idents(integrated) <- "cell_cycle"
-
-    return(integrated)
+    seurat_object <- Seurat::CellCycleScoring(
+        object = seurat_object,
+        g2m.features = g2m_genes,
+        s.features = s_genes
+    )
+    
+    return(seurat_object)
 }
