e0234e52873e4b5a6b0f0fe7883b4d1df9aad3ac
Denis O'Meally <domeally@coh.org>
2023-01-03 12:59:00 -0800

    use sample_id instead of library_id

--- R/run_nfcore.R
+++ R/run_nfcore.R
@@ -56,7 +56,7 @@ run_nf_core_rnaseq <- function(run_folder, sample_sheet, ref_genome, qc = TRUE) 
         # if theres no run script, make one and submit it to the cluster
     } else if (!file.exists(glue::glue("{run_path}/run_{run_mode}_{ref_genome}.sh"))) {
         sample_sheet |>
-            dplyr::select(sample = library_id, fastq_1, fastq_2, strandedness) |>
+            dplyr::select(sample = sample_id, fastq_1, fastq_2, strandedness) |>
             readr::write_csv(paste0(run_path, "/sample_sheet.csv"))
 
         # make a sbatch script
@@ -69,6 +69,9 @@ run_nf_core_rnaseq <- function(run_folder, sample_sheet, ref_genome, qc = TRUE) 
 #SBATCH --get-user-env=L
 #SBATCH -e {run_path}/slurm-%j.err
 #SBATCH -o {run_path}/slurm-%j.out
+
+source /opt/modulefiles/bashrc.local
+PATH=~/bin:$PATH
 
 cd {run_path}
 module load singularity/3.7.0
