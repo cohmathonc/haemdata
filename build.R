7b7f6813c863c77b8df1869fb9eb9f8c674cc97a
Denis O'Meally <domeally@coh.org>
2023-02-03 05:01:07 +0000

    fix function names

--- build.R
+++ build.R
@@ -71,7 +71,7 @@ pkgdown::build_site()
 # Draft a release for GitHub
 usethis::use_github_release()
 
-fs.copy
+fs::file_copy(here::here("preprocessing/_targets.html"), "~/MHO/haemdata-www", overwrite = TRUE)
 
 devtools::document()
 pkgdown::build_reference()
